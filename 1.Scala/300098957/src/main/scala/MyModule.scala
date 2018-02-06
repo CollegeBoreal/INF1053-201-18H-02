b240702862cb63317af6c307a39fa378f5f28b3d
Setrar <setrar@valiha.com>
2018-02-06 13:58:53 -0500

    Rajout de Fibonacci

--- 1.Scala/300098957/src/main/scala/MyModule.scala
+++ 1.Scala/300098957/src/main/scala/MyModule.scala
@@ -6,12 +6,19 @@ object MyModule {
 
   def factorial(n: Int): Int = {
 
-    def go(n: Int, acc: Int): Int = {
+    def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
       else go(n-1, n * acc)
-    }
 
     go(n, 1)
+  }
+
+  def fib(n: Int): Int = {
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n <= 0) prev
+      else loop(n - 1, cur, prev + cur)
+
+    loop(n, 0, 1)
   }
 
   private def formatResult(name: String, x: Int, f: Int => Int) = {
@@ -22,6 +29,7 @@ object MyModule {
   def main(args: Array[String]): Unit = {
     println(formatResult("absolute value",-42,abs))
     println(formatResult("factorial",3,factorial))
+    println(formatResult("fib",5,fib))
   }
 
 }

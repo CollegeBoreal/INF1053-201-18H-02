31e6b27bf7c63a7da0c056abf3707aa2adbb6a58
Mamoudou Sow <sowmkb@yahoo.com>
2018-02-08 16:16:04 -0500

    data structure

--- 1.Scala/3.Data/300102828/src/main/scala/list.scala
+++ 1.Scala/3.Data/300102828/src/main/scala/list.scala
@@ -5,21 +5,53 @@ case class Cons[+A](head: A, tail :List[A]) extends List[A]
 
 object List {
 
-  def sum(ints: List[Int]): Int = ints  match {
+  def sum(ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
   }
-  def product(ds: List[Double]): Double = ds  match {
+
+  def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
     case Cons(x, xs) => x * product(xs)
   }
 
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+
 
   def main(args: Array[String]): Unit = {
-    val ex1: List[Int] = Nil;println(List.sum(ex1))
-    val ex2 : List[Int] = Cons(1,Nil);println(List.sum(ex2))
-    val ex3 : List[Int] = Cons(4,Cons(11,Nil));println(List.sum(ex3))
-    val ex4 : List[Int] = Cons(4,Cons(11,Cons(20,Nil)));println(List.sum(ex4))
+
+    //somme
+    val ex1: List[Int] = Nil;
+    println(List.sum(ex1))
+    val ex2: List[Int] = Cons(1, Nil);
+    println(List.sum(ex2))
+    val ex3: List[Int] = Cons(4, Cons(11, Nil));
+    println(List.sum(ex3))
+    val ex4: List[Int] = Cons(4, Cons(11, Cons(20, Nil)));
+    println(List.sum(ex4))
+    //produit
+    val ex5: List[Double] = Nil;
+    println(List.product(ex5))
+    val ex6: List[Double] = Cons(1.0, Cons(0.0, Nil));
+    println(List.product(ex6))
+    val ex7: List[Double] = Cons(4, Cons(11, Cons(20, Nil)));
+    println(List.product(ex7))
+
+
+    //constructeur
+    val ex8: List[Int] = List(1, 2, 3, 4, 5);println(List.sum(ex8))
+    val x = List(1,2,3,4,5) match {
+      case Cons(x,Cons(2, Cons(4 , _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3 ,Cons(4, _)))) => x + y
+      case Cons(h,t) => h + sum(t)
+      case _ => 101
+    }
+println(x)
   }
+
 }

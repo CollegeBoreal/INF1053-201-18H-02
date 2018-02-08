2dc4f593713aa977d3b7f177ae4adef0a0c9f5e1
Mamoudou Sow <sowmkb@yahoo.com>
2018-02-08 15:24:57 -0500

    Data structure suit

--- 1.Scala/3.Data/300102828/src/main/scala/list.scala
+++ 1.Scala/3.Data/300102828/src/main/scala/list.scala
@@ -9,8 +9,17 @@ object List {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
   }
+  def product(ds: List[Double]): Double = ds  match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
 
   def main(args: Array[String]): Unit = {
-    println(List(1,2,3,4).sum)
+    val ex1: List[Int] = Nil;println(List.sum(ex1))
+    val ex2 : List[Int] = Cons(1,Nil);println(List.sum(ex2))
+    val ex3 : List[Int] = Cons(4,Cons(11,Nil));println(List.sum(ex3))
+    val ex4 : List[Int] = Cons(4,Cons(11,Cons(20,Nil)));println(List.sum(ex4))
   }
 }

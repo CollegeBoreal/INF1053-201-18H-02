a6316fc1796a81277984487be9ea1c01e2b2f1b0
Setrar <setrar@valiha.com>
2018-02-08 15:03:56 -0500

    Pause

--- 1.Scala/3.Data/300098957/src/main/scala/List.scala
+++ 1.Scala/3.Data/300098957/src/main/scala/List.scala
@@ -9,9 +9,11 @@ object List {
     case Cons(x, xs) => x + sum(xs)
   }
 
-
   def main(args: Array[String]): Unit = {
-    println(List(1,2,3,4).sum)
+    val ex1: List[Int] = Nil; println(List.sum(ex1))
+    val ex2: List[Int] = Cons(1, Nil); println(List.sum(ex2))
+    val ex3: List[Int] = Cons(4, Cons(11,Nil)); println(List.sum(ex3))
+    val ex4: List[Int] = Cons(4, Cons(11,Cons( 20, Nil))); println(List.sum(ex4))
   }
 
 }

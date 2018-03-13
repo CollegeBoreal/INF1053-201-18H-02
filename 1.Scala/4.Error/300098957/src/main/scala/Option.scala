2cef4912acc4166f6b6f4b56aef9508ce0732eea
Setrar <setrar@valiha.com>
2018-03-13 16:56:33 -0400

    Option fin

--- 1.Scala/4.Error/300098957/src/main/scala/Option.scala
+++ 1.Scala/4.Error/300098957/src/main/scala/Option.scala
@@ -28,14 +28,42 @@ object Option {
 
   def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m,2))))
 
-  def main(args: Array[String]): Unit = {
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch { case e: Exception => None}
 
+  def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double =  (age + numberOfSpeedingTickets) /  5
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] = a flatMap (aa => b map (bb => f(aa, bb)))
+
+  def map2_[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
+
+  def parseInsuranceQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
+    val optAge: Option[Int] = Try {age.toInt}
+    val optTickets: Option[Int] = Try {numberOfSpeedingTickets.toInt}
+    map2_(optAge, optTickets)(insuranceRateQuote)
+  }
+
+  def sequence[A](as: List[Option[A]]): Option[List[A]] = as match {
+    case Nil => Some(Nil)
+    case x::xs =>  x flatMap (xx => sequence(xs) map ( xxs => xx :: xxs))
+  }
+
+  def sequence_[A](as: List[Option[A]]): Option[List[A]] = as match {
+    case Nil => Some(Nil)
+    case x::xs =>  for ( xx <- x; xxs <- sequence(xs)) yield xx :: xxs
+  }
+
+  def main(args: Array[String]): Unit = {
     assert(mean(Seq(2.0,2.0,2.0))==Some(2.0))
     assert(Some(2.0).get==2.0)
     assert(mean(Seq())==None)
     val x = mean(Seq()) match { case Some(x) => x; case None => 0}; assert(x == 0)
     val y = mean(Seq(2.0)) match {case Some(x) => x case None => 0}; assert(y == 2.0)
-
     assert(Some(2.0).map((x:Double) => x + 1)==Some(3.0))
     assert(Some(2).getOrElse(1)==2); assert(None.getOrElse(1)==1)
     assert(Some(2.0).flatMap((x:Double) => None)==None)
@@ -43,6 +71,11 @@ object Option {
     assert(Some(2).orElse(Some(1))==Some(2)); assert(None.orElse(Some(1))==Some(1))
     assert(Some(5).filter(_ == 5)==Some(5))
     assert(variance(Seq(4.9,7.1,9.8,3.7,5.1))==Some(4.577600000000001))
+    assert(Try("elf".toInt)==None)
+    assert(parseInsuranceQuote("25","10")==Some(7.0)); assert(parseInsuranceQuote("elf","9")==None)
+
+    assert(sequence(List(Some(1),Some(2),Some(3)))==Some(List(1,2,3)))
+    assert(sequence_(List(Some(1),Some(2),Some(3)))==Some(List(1,2,3)))
 
   }
 }

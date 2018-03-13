73c71ee6a9a133ee852761f933b7b0722e7e3ac9
Mamoudou Sow <sowmkb@yahoo.com>
2018-03-13 16:50:03 -0400

    option

--- 1.Scala/4.Error/300102828/src/main/scala/Option.scala
+++ 1.Scala/4.Error/300102828/src/main/scala/Option.scala
@@ -19,7 +19,6 @@ sealed trait Option[+A] {
     case _ => None
   }
 
-
 }
 case class Some[+A](get:A) extends  Option[A]
 case object None extends Option[Nothing]
@@ -36,6 +35,38 @@ object Option {
 
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def Try[A](a: => A): Option[A] =
+    try Some(a)
+    catch {case e: Exception => None}
+
+  def insuranceRateQuote( age: Int, numberOfSpeedingTickets: Int): Double = (age + numberOfSpeedingTickets) / 5
+
+  def map2[A,B,C](a: Option[A],b: Option[B])(f:(A,B) => C): Option[C] = a flatMap(aa => b map(bb => f(aa,bb)))
+
+  def map2_[A,B,C](a: Option[A],b: Option[B])(f:(A,B) => C): Option[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa,bb)
+
+
+  def parseInsuranceRateQuote( age: String, numberOfSpeedingTickets: String): Option[Double]= {
+    val optAge: Option[Int] = Try {age.toInt}
+    val optTicket: Option[Int] = Try  { numberOfSpeedingTickets.toInt}
+   map2_(optAge, optTicket)(insuranceRateQuote)
+  }
+
+  def sequence[A](as: List[Option[A]]): Option[List[A]] =  as match {
+    case Nil => Some(Nil)
+    case x :: xs => x flatMap (xx => sequence(xs) map (xxs => xx::xxs))
+  }
+
+  def sequence_[A](as: List[Option[A]]): Option[List[A]] =  as match {
+    case Nil => Some(Nil)
+    case x :: xs => for (xx <- x; xxs <- sequence(xs)) yield xx :: xxs
+  }
+
 
 
   def main(args: Array[String]): Unit = {
@@ -55,5 +86,11 @@ object Option {
     assert(Some(2).orElse(Some(1))== Some(2)); assert(None.orElse(Some(1))== Some(1))
     assert(Some(5).filter(_==5)== Some(5))
     assert(variance(Seq(4.9,7.1,9.8,3.7,5.1))== Some(4.577600000000001))
+    assert(Try("elf".toInt)==None)
+    assert(parseInsuranceRateQuote("25","10")== Some(7.0)); assert(parseInsuranceRateQuote("elf","9")== None)
+    assert(sequence(List(Some(1),Some(2),Some(3)))== Some(List(1,2,3)))
+    assert(sequence_(List(Some(1),Some(2),Some(3)))== Some(List(1,2,3)))
+
   }
+
 }

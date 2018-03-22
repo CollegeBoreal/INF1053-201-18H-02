935089be71f99ce1dc57ffc362c0c6975323658d
Mamoudou Sow <sowmkb@yahoo.com>
2018-03-22 15:40:28 -0400

    Stream

--- 1.Scala/5.Lazy/300102828/src/main/scala/Stream.scala
+++ 1.Scala/5.Lazy/300102828/src/main/scala/Stream.scala
@@ -1,4 +1,22 @@
-sealed trait Stream[+A]
+sealed trait Stream[+A]{
+  def heaOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h,t) => Some(h())
+  }
+  def toListRecursive: List[A] = this match {
+    case Cons(h,t) => h() :: t().toListRecursive
+    case _ => List()
+  }
+
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Cons(h,t) => go(t(), h() :: acc)
+      case _ => acc
+    }
+    go(this, List()).reverse
+  }
+}
 case object Empty extends Stream[Nothing]
 case class Cons[+A] (h: () => A ,t: () => Stream[A]) extends Stream[A]
 
@@ -14,10 +32,14 @@ object Stream {
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
-  def heaOption: Option[A] = this match {
-    case Empty => None
-    case Cons(h,t) => Some(h(n))
+  def main(args: Array[String]): Unit = {
+
+    assert(empty.heaOption==None)
+    assert(Stream(1,2,3,4).heaOption==Some(1))
+    assert(Stream(1,2,3,4).toListRecursive==List(1,2,3,4))
+    assert(Stream(1,2,3,4).toList==List(1,2,3,4))
   }
+
 
 
 }

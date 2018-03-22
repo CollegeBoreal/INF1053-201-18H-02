92ac3484b03a9cb38a67924ed90f507a12875012
Setrar <setrar@valiha.com>
2018-03-22 16:10:02 -0400

    é

--- 1.Scala/5.Lazy/300098957/src/main/scala/Stream.scala
+++ 1.Scala/5.Lazy/300098957/src/main/scala/Stream.scala
@@ -1,1 +1,40 @@
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h,t) => Some(h())
+  }
+  def toListRecursive: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toListRecursive
+  }
+  def toList: List[A] = {
+    @annotation.tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] = s match {
+      case Empty => acc
+      case Cons(h, t) => go(t(), h() :: acc)
+    }
+    go(this, Nil).reverse
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons( () => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  def main(args: Array[String]): Unit = {
+    assert(Empty.headOption==None)
+    assert(Stream(1,2,3,4).headOption==Some(1))
+    assert(Stream(1,2,3,4).toListRecursive==List(1,2,3,4))
+    assert(Stream(1,2,3,4).toList==List(1,2,3,4))
+  }
+}

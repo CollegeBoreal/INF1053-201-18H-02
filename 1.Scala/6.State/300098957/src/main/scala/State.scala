e7e14082f9de5b252beee159dfdfa4a65809e5e0
SetraR <setrar@valiha.com>
2018-03-24 14:00:11 -0400

    mingling

--- 1.Scala/6.State/300098957/src/main/scala/State.scala
+++ 1.Scala/6.State/300098957/src/main/scala/State.scala
@@ -2,21 +2,14 @@
 import State._
 
 case class State[S, +A](run: S => (A, S)) {
-
-  def map[B](f: A => B): State[S, B] =
-    flatMap(a => unit(f(a)))
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    flatMap(a => sb.map(b => f(a, b)))
-  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
-    val (a, s1) = run(s)
-    f(a).run(s1)
-  })
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = for (a <- this; b <- sb) yield f(a,b)
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => { val (a, s1) = run(s); f(a).run(s1) })
 }
 
 object State {
 
-  def unit[S, A](a: A): State[S, A] =
-    State(s => (a, s))
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
 
   // This implementation uses a loop internally and is the same recursion
   // pattern as a left fold. It is quite common with left folds to build

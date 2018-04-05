9bc04b37d19a32b5c8de01971d663c2d03bff207
Setra <setrar@valiha.com>
2018-04-05 18:55:24 -0400

    Applicative

--- 1.Scala/6.State/300098957/src/main/scala/isolated/Candy.scala
+++ 1.Scala/6.State/300098957/src/main/scala/isolated/Candy.scala
@@ -6,35 +6,53 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-import scalaz._, std.list._, syntax.traverse._
-
+import scalaz._, std.list._, State._
 
 object Candy {
 
-  val m: State[Machine, (Int,Int)] = State[Machine, (Int,Int)]{
-    m => (Machine(false, m.candies + 1, m.coins + 1 ), (m.candies + 1, m.coins + 1))
-  }
-
-  def updateRule(i: Input): State[Machine, (Int,Int)] = State[Machine, (Int,Int)]{
-    (m: Machine) => (i,m) match {
-      case (_, Machine(_, 0, _))        => (m, (m.candies, m.coins))
-      case (Coin, Machine(false, _, _)) => (m, (m.candies, m.coins))
-      case (Turn, Machine(true, _, _))  => (m, (m.candies, m.coins))
-      case (Coin, Machine(true, candy, coin)) => (Machine(locked = false, candy, coin + 1 ), (candy, coin + 1))
-      case (Turn, Machine(false, candy, coin)) => (Machine(locked = true, candy - 1, coin),(candy - 1, coin))
-    }
+  def updateRule(i: Input): Machine => Machine = (s: Machine) => (i, s) match {
+    case (_, Machine(_, 0, _))        => s
+    case (Coin, Machine(false, _, _)) => s
+    case (Turn, Machine(true, _, _))  => s
+    case (Coin, Machine(true, candy, coin)) => Machine(locked = false, candy, coin + 1)
+    case (Turn, Machine(false, candy, coin)) => Machine(locked = true, candy - 1, coin)
   }
 
   type MachineState[A] = State[Machine,A]
+  val app = Applicative[MachineState]
+
+  // To generate the first 5 Random integers
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+    for {
+      _ <- app.sequence(inputs.map(modify[Machine] _ compose updateRule))
+      s <- get
+    } yield (s.coins, s.candies)
 
   def main(args: Array[String]): Unit = {
 
-    assert(m.eval(Machine(locked = true, 0, 0))==(1,1))
-    assert(m.exec(Machine(locked = true, 0, 0))==Machine(locked = false, 1, 1))
-    assert(updateRule(Turn).exec(Machine(locked = false, 1, 0))==Machine(locked = true, 0, 0))
-    assert(updateRule(Coin).eval(Machine(locked = true, 1, 0))==(1, 1))
+    // Inserting a coin into a locked machine will cause it to unlock if there’s any candy left.
+    val (state1,a) = simulateMachine(List(Coin)).run(Machine(locked = true, 1, 0))
+    assert(!state1.locked)
+    assert(a==(1,1)) // $1, 1 candy left
 
-    assert(List(Coin, Turn).traverse[MachineState,(Int,Int)](updateRule).eval(Machine(locked = true, 1, 0))==List((1,1), (0,1)))
+    // Turning the knob on an unlocked machine will cause it to dispense candy and become locked.
+    val (state2,b) = simulateMachine(List(Turn)).run(Machine(locked = false, 1, 1))
+    assert(state2.locked)
+    assert(state2.candies==0)
+    assert(b==(1,0)) // $1, No candies left
+
+    // the Input Machine has 1 coin and 1 candy, and 1 candy is successfully bought
+    val (state3,x) = simulateMachine( Coin :: Turn :: Nil).run(Machine(locked = true, 1, 1))
+    assert(state3.locked)
+    assert(state3.candies==0)
+    assert(x==(2,0)) // $2, No candies left
+
+    // the Input Machine has 10 coins and 5 candies, and a total of 4 candies are successfully bought
+    val (state4,y) = simulateMachine(List(Coin,Turn,Coin,Turn,Coin,Turn,Coin,Turn)).run(Machine(locked = true, 5, 10))
+    assert(state4.locked)
+    assert(state4.coins==14)
+    assert(state4.candies==1)
+    assert(y==(14,1)) // $14, 1 candy left
 
   }
 
